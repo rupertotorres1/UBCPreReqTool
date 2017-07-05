f08ce4904286abf5f1a1c98a60722424553e953e
Ruperto Torres <rupertotorres1@gmail.com>
2017-07-05 16:44:46 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+from pickle import load
+dict = load(open("dictCoursesPreCoReqs.p", "rb"))
+
+print ("Welcome! This tool helps you find out which courses require the given course as a pre-req or co-req. I hope it is useful.")
+
+def interaction():
+	print ("")
+
+	courseInput = input("Which course would you like to look up? ").upper()
+	print("")
+	preCoReqFor = []
+
+	if (not(courseInput in dict)):
+		print ("That is not a valid course")
+		interaction()
+	else:
+		for course, preCoReqs in dict.items():
+			if (courseInput in preCoReqs):
+				preCoReqFor.append(course)
+
+		print(courseInput, "is a pre-req or co-req for:")
+		print(preCoReqFor)
+		print("")
+
+		doAgain = input("Would you like to look up another course?[yes/no] ").lower()
+		if (doAgain == "yes"):
+			interaction()
+
+interaction()
+
