caa601a1dd1a8bbf1943096b1d0f378163594578
Andres Ruperto Torres <rupertotorres1@gmail.com>
2017-08-07 13:53:17 -0500

    Update main.py
    Comments

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ except NameError:
 
 import sys
 from pickle import load
+# Load dictionary with courses and their pre-requisites and co-requisites
 dict = load(open("dictCoursesPreCoReqs.p", "rb"))
 
 print ("Welcome! This tool helps you find out which courses require the given course as a pre-req or co-req. I hope it is useful.")
@@ -17,9 +18,12 @@ def interaction():
 		print("")
 		pre_co_req_for = []
 
+		# If the course that the user provided is not in the loaded dictionary, ask again
 		if (not(course_input in dict)):
 			print ("That is not a valid course")
 			interaction()
+		# Else, search the courses for which the provided course is a pre-requisite or co-requisite 
+		# and add them to a list.
 		else:
 			for course, pre_co_reqs in dict.items():
 				if (course_input in pre_co_reqs):
